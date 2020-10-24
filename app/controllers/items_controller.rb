fe928a216f1c954e85456f8b2a5646dce9eb02a2
tomoheko <dekirumadeyamenai@gmail.com>
2020-10-24 15:52:07 +0900

    商品削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
-  before_action :item_id, only: [:edit, :show, :update]
+  before_action :item_id, only: [:edit, :show, :update, :destroy]
 
   def new
     @item = Item.new
@@ -36,6 +36,12 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
 
 
   private
